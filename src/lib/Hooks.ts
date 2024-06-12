b397c428f5963c5b20dd5636fa6b531ddc6b1571
Craig Johnson <craigdev937@gmail.com>
2024-06-12 01:48:51 -0400

    Updated the Hooks.ts file,.

--- src/lib/Hooks.ts
+++ src/lib/Hooks.ts
@@ -1,10 +1,8 @@
 import { useSelector, useStore, useDispatch } from "react-redux";
-import type { TypedUseSelectorHook } from "react-redux";
 import type { AppStore, RootState, AppDispatch } from "./store";
 
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: 
-    TypedUseSelectorHook<RootState> = useSelector;
-export const useAppStore: () => AppStore = useStore;
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector = useSelector.withTypes<RootState>();
+export const useAppStore = useStore.withTypes<AppStore>();
 
 
